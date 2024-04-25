6f661952e05d85a70d98e6a1aab5060cbeaf95b4
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2024-04-25 00:02:01 +0000

    Basic data information

--- data_exploration.py
+++ data_exploration.py
@@ -8,3 +8,15 @@ adults = 'https://raw.githubusercontent.com/csbfx/advpy122-data/master/adult.csv
 data = pd.read_csv(adults, na_values=['?'])
 
 print(data)
+
+# Basic data information
+data_info = data.info()
+
+# Display the first few rows
+first_few_rows = data.head()
+
+# Print categorical fields and perform value counts
+categorical_columns = data.select_dtypes(include=['object']).columns
+value_counts = {column: data[column].value_counts() for column in categorical_columns}
+
+print(data_info, first_few_rows, categorical_columns, value_counts)
